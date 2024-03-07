d9b5a542d9c81dc82df1ee02098439aa853e197c
Patrick <patrick.ojeda.91@gmail.com>
2024-03-06 22:33:07 -0300

    funcion de aumentar espacio en web agregada a script

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+const edad_profe = 29
+const mensaje_2 = "el profe tiene " + edad_profe + " años"
+
+console.log (mensaje_2)
+//-
+(function aumentar_espacio () {
+  $('h1').html($('h1').html().substring(0,8))
+  $('main h2.h3').css('font-size', '1rem')
+  $('img[alt="Instituto Praxis"]').css('display', 'none')
+  $('header section .col-8.col-md.mb-1').css('display', 'none')
+  $('header .col-4.col-md.mb-1.text-md-right').css('display', 'none')
+})()
